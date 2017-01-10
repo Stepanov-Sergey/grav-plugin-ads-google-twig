95c0eff09bbf1eb58fefabaecba4ae9edfdacca5
Sergey Stepanov <sergey.stepanov@lenta.ru>
2017-01-10 13:42:04 +0300

    Update adsgoogletwig.php

--- adsgoogletwig.php
+++ adsgoogletwig.php
@@ -15,7 +15,7 @@ class GalleryPlugin extends Plugin
      */
     public function onTwigExtensions()
     {
-        require_once(__DIR__ . '/adsgoogletwig.php');
+        require_once(__DIR__ . '/adsgoogletwigext.php');
         $this->grav['twig']->twig->addExtension(new adsgoogletwig()); 
     }
 }
