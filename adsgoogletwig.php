4f711fceb1398a7851edacb3ee8b3fbb883478d0
Sergey Stepanov <sergey.stepanov@lenta.ru>
2017-01-10 13:41:22 +0300

    Create adsgoogletwig.php

---
+++ adsgoogletwig.php
@@ -0,0 +1,21 @@
+<?php
+namespace Grav\Plugin;
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+class GalleryPlugin extends Plugin
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onTwigExtensions' => ['onTwigExtensions', 0]
+        ];
+    }
+    /**
+     * Initialize the plugin
+     */
+    public function onTwigExtensions()
+    {
+        require_once(__DIR__ . '/adsgoogletwig.php');
+        $this->grav['twig']->twig->addExtension(new adsgoogletwig()); 
+    }
+}
